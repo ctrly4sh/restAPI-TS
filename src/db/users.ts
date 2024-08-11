b4468606ca0bf7badb429a0883758ae8faea8690
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-08-11 23:12:22 +0530

    init

---
+++ src/db/users.ts
@@ -0,0 +1,40 @@
+import exp from "constants";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  authentication: {
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    salt: {
+      type: String,
+      select: false,
+    },
+    sessionToken: {
+      type: String,
+      Select: false,
+    },
+  },
+});
+
+export const userModel =  mongoose.model("user" , UserSchema)
+
+// //Read
+// export const getUsers = () => userModel.find();
+// export const getUserByEmail = (email : string) => userModel.findOne({email})
+// export const getUserBySessionToken = (sessionToken : string) => userModel.findOne({
+//   'authentication.sessionToken' : sessionToken
+// })
+// export const getUserById = (id : string) => userModel.findById({id})
+
+// export const createUser = (values : Record<string , any>) => new userModel(values).save().then((user)=> user.toObject())
