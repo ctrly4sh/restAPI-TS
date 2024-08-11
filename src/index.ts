12db29d47ed064bd53e063f4f29d1815cf9c5ea4
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-08-11 23:12:31 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
 import express from "express";
-import http from "http"
+import http from "http";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser"
-import compression from "compression"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app = express()
+dotenv.config();
 
-app.use(cors({
-    credentials : true,
-}))
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.use(compression())
-app.use(cookieParser())
-app.use(bodyParser.json())
+const app = express();
 
-const server = http.createServer(app)
+const mongoDBConnection = mongoose.connect(MONGO_URL);
 
-server.listen(4567 , ()=>{
-    console.log("Server started at https://localhost:")
-})
+mongoDBConnection
+  .then(() => console.log("Mongo DB connected"))
+  .catch((eror: Error) => console.log(`Error occured ${eror}`));
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server started at https://localhost:${PORT}`);
+});
+
